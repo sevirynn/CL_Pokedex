a604beb6d9535e0eb876c7d02314c7da3405d1b8
sevirynn <sevirynn@protonmail.com>
2024-06-09 14:48:06 -0400

    started planning app logic

---
+++ Pokedex.java
@@ -0,0 +1,20 @@
+public class Pokedex{
+
+    public static void main (String[] args){
+       System.out.println("******************************************************************************************************************************************************");
+       System.out.println("------------------------------------Please extend your terminal window so the stars above appear on a single line-------------------------------------");
+    }
+
+    //Request user input in form of number 001-151 or name "bulbasaur"
+        //verify that number is three digits if not - try again
+        //take name and convert to lower
+    
+    //Search */Pokedex_Entries for the information provided
+        //if found print reader the entry line by line to the terminal
+        //if not return an error message
+
+    //Ask if they'd like to search again
+        //Yes return to input
+        //No exit application
+
+}
